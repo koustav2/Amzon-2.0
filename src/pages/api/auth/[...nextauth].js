98ecf742f83025689fb311b30ee58f06f3d080c8
koustav <koustavm.aeie2018@nsec.ac.in>
2023-06-14 22:05:02 +0530

    updated🔯🔯🌡️🌡️🍉🍉🍉

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -14,15 +14,15 @@ export const authOptions = {
         Google({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
         })
+        // GithubProvider({
+        //     clientId: process.env.GITHUB_ID,
+        //     clientSecret: process.env.GITHUB_SECRET,
+        // }),
+        // FacebookProvider({
+        //     clientId: process.env.FACEBOOK_CLIENT_ID,
+        //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+        // })
     ],
     // // adapter: FirebaseAdapter(db)
     // adapter: TypeORMLegacyAdapter({
