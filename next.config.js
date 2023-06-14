db3020991af551430a0c4dfb9e18c67dfdb23985
koustav <koustavm.aeie2018@nsec.ac.in>
2023-06-14 20:07:52 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     images: {
-        domains: ['links.papareact.com','images.ctfassets.net', 'images.unsplash.com','fakestoreapi.com'],
+        domains: ['links.papareact.com','images.ctfassets.net', 'images.unsplash.com','fakestoreapi.com','www.nicepng.com'],
     },
     env : {
         stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`
